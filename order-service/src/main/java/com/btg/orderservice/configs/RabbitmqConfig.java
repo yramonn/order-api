c74fe4df4448cdc8bf664ae34f713a3befb39904
Ramon <ramonsilva12305@outlook.com>
2025-04-07 00:27:32 -0300

    infra: Dockerfile and docker-compose

--- order-service/src/main/java/com/btg/orderservice/configs/RabbitmqConfig.java
+++ order-service/src/main/java/com/btg/orderservice/configs/RabbitmqConfig.java
@@ -2,9 +2,11 @@ package com.btg.orderservice.configs;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,6 +17,29 @@ public class RabbitmqConfig {
 
     public RabbitmqConfig(CachingConnectionFactory cachingConnectionFactory) {
         this.cachingConnectionFactory = cachingConnectionFactory;
+    }
+
+    @Value("${order.broker.exchange.orderEventExchange}")
+    private String orderEventExchange;
+
+    @Value("${order.broker.queue.orderEventQueue.name}")
+    private String orderEventQueue;
+
+    @Bean
+    public FanoutExchange fanoutExchange() {
+        return new FanoutExchange(orderEventExchange);
+    }
+
+    @Bean
+    public Queue orderQueue() {
+        return new Queue(orderEventQueue, true);
+    }
+
+    @Bean
+    public Binding bindingOrderQueueToExchange() {
+        return BindingBuilder
+                .bind(orderQueue())
+                .to(fanoutExchange());
     }
 
     @Bean
@@ -30,5 +55,4 @@ public class RabbitmqConfig {
         objectMapper.registerModule(new JavaTimeModule());
         return new Jackson2JsonMessageConverter(objectMapper);
     }
-
 }
